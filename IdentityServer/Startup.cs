cb7d1f73dbdc2eac04c05fac0697dccdfd3e9f73
张良 <562974055@qq.com>
2019-03-01 17:18:48 +0800

    1

--- IdentityServer/Startup.cs
+++ IdentityServer/Startup.cs
@@ -5,30 +5,43 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace IdentityServer
 {
     public class Startup
     {
+        IConfiguration configuration;
+        IHostingEnvironment env;
+        public Startup(IConfiguration configuration, IHostingEnvironment env)
+        {
+            this.configuration = configuration;
+            this.env = env;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            var identityBuilder = services.AddIdentityServer()
+                                        .AddInMemoryIdentityResources(Config.GetIdentityResources())
+                                        .AddInMemoryApiResources(Config.GetApis())
+                                        .AddInMemoryClients(Config.GetClients());
+
+            if (env.IsDevelopment())
+            {
+                identityBuilder.AddDeveloperSigningCredential();
+            }
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseIdentityServer();
         }
     }
 }
