8390e50eab3fc9555df9468f98a9294a1b4a3cad
张良 <562974055@qq.com>
2019-03-11 11:29:49 +0800

    submit test nginx www root folder

---
+++ nginx_wwwroot/debug/precache-manifest.2089e96798f87c3d104769e3eb5c50a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "25d4f05307542a1466ca",
+    "url": "/emoji-search/static/js/runtime~main.b99b63e8.js"
+  },
+  {
+    "revision": "a8e4d4189e922dffcbda",
+    "url": "/emoji-search/static/js/main.e24c2c1c.chunk.js"
+  },
+  {
+    "revision": "d83a64953a3b1e0dc10e",
+    "url": "/emoji-search/static/js/2.f154ceab.chunk.js"
+  },
+  {
+    "revision": "a8e4d4189e922dffcbda",
+    "url": "/emoji-search/static/css/main.badb4ae1.chunk.css"
+  },
+  {
+    "revision": "d83a64953a3b1e0dc10e",
+    "url": "/emoji-search/static/css/2.b272455b.chunk.css"
+  },
+  {
+    "revision": "c9c24f1232ca6cd2fe9b6b8bb7af13c5",
+    "url": "/emoji-search/index.html"
+  }
+];
